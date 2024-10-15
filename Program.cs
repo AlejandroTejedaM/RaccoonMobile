e22cf141b41fa334839efdc08c88ced539bdf627
Kintx2227 <alex2227@hotmail.com>
2024-10-15 16:03:42 -0600

    Added initial files --Running

--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
-using RaccoonMobile.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddSignalR();
+// Agregar servicios al contenedor.
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configurar la canalización de solicitudes HTTP.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
@@ -22,7 +23,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapHub<NotificacionesHub>("/chatHub");
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
