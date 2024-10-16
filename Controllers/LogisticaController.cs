567b49048464055f1abfc48093f9904db09535df
Kintx2227 <alex2227@hotmail.com>
2024-10-16 12:59:19 -0600

    Added controller for logistica

---
+++ Controllers/LogisticaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RaccoonMobile.Controllers;
+
+public class LogisticaController : Controller
+{
+    // GET
+    public IActionResult IndexLogistica()
+    {
+        return View();
+    }
+}
